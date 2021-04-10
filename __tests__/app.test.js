1b74e409dad4eba8e51fa6a12c0f20284c634c9a
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-04-09 17:55:14 -0700

    add post route and test for players

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,26 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
+const sequelizeDb = require('../lib/utils/sequelize');
 const request = require('supertest');
 const app = require('../lib/app');
 
 describe('sequalize-practice routes', () => {
   beforeEach(() => {
-    return setup(pool);
+    // drops tables before each test
+    return sequelizeDb.sync({ force: true });
+  });
+
+  it('creates a player using POST', () => {
+    return request(app)
+      .post('/api/v1/players')
+      .send({ name: 'Alex Morgan', careerGoals: 108, teams: ['Team USA', 'Orlando Pride'] })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: 1,
+          name: 'Alex Morgan',
+          careerGoals: 108,
+          teams: ['Team USA', 'Orlando Pride'],
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String)
+        });
+      });
   });
 });
