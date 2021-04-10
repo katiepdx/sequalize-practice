e53fff58feb840e95168873040421a1f0c0ea277
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-04-09 19:56:34 -0700

    add get all players route and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 const sequelizeDb = require('../lib/utils/sequelize');
 const request = require('supertest');
 const app = require('../lib/app');
+const Player = require('../lib/models/Player');
 
 describe('sequalize-practice routes', () => {
   beforeEach(() => {
@@ -21,6 +22,38 @@ describe('sequalize-practice routes', () => {
           createdAt: expect.any(String),
           updatedAt: expect.any(String)
         });
+      });
+  });
+
+  it('gets all players using GET', () => {
+    // bulkCreate (sequelize) - adds multiple items at a time 
+    Player.bulkCreate([
+      { name: 'Alex Morgan', careerGoals: 108, teams: ['Team USA', 'Orlando Pride'] },
+      { name: 'Carli Lloyd', careerGoals: 124, teams: ['Team USA', 'Gotham FC'] }
+    ]);
+    return request(app)
+      .get('/api/v1/players')
+      .then(res => {
+        expect(res.body).toEqual(
+          [
+            {
+              id: expect.any(Number),
+              name: 'Alex Morgan',
+              careerGoals: 108,
+              teams: ['Team USA', 'Orlando Pride'],
+              createdAt: expect.any(String),
+              updatedAt: expect.any(String)
+            },
+            {
+              id: expect.any(Number),
+              name: 'Carli Lloyd',
+              careerGoals: 124,
+              teams: ['Team USA', 'Gotham FC'],
+              createdAt: expect.any(String),
+              updatedAt: expect.any(String)
+            }
+          ]
+        );
       });
   });
 });
